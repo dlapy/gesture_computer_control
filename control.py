e17488feaab634f9fa56c195cc04401a12c66a3a
sem <evelisko@mail.ru>
2023-03-05 13:02:20 +0400

    change  algoritm  for image scale

--- control.py
+++ control.py
@@ -4,7 +4,7 @@ import cv2
 import pyautogui as pag
 
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 hands_1 = mp.solutions.hands.Hands(static_image_mode=False, max_num_hands=1, min_tracking_confidence=0.5, min_detection_confidence=0.5)
 
 
--- krolik.py
+++ krolik.py
@@ -1,8 +1,13 @@
 from control import mous_control
 
 def main():
+    counter = 0
     while True:
-        mous_control()
+        counter+=1
+        if counter>3:
+            mous_control()
+            counter=0
+        
 
 
 if __name__ == "__main__":
